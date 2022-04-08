038b3f366001bbc9125b114a5199025c03817c8a
soliye <soliy@gmail.com>
2022-04-08 04:24:54 -0700

    task 7 if loop

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char alpha;
+for (alpha = 122; alpha >= 97; alpha--)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
