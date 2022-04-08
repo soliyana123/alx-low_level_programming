a97541b2babd0b1295d60476bb8f6a4f15c58d44
soliye <soliy@gmail.com>
2022-04-08 04:48:14 -0700

    task 4 if loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main -Entry point
+*main - Entry point
 *
 *Return: Always 0 (Success)
 */
@@ -11,9 +11,9 @@ char alpha;
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 if (alpha === 'q')
-	continue;
+continue;
 else if (alpha == 'e')
-	continue;
+continue;
 putchar(alpha);
 }
 putchar('\n');
