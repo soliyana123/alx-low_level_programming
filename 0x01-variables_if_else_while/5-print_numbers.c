1a96a38b5bc83720b333dfd85020b881ee0f8e99
soliye <soliy@gmail.com>
2022-04-08 04:06:47 -0700

    task 5 if loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
