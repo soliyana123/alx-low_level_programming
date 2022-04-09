2fb9355941eda1453a6a4c7545d9d552c5f790bd
soliye <soliy@gmail.com>
2022-04-09 00:59:04 -0700

    task 10 is not good for me

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,20 +2,19 @@
 /**
 * main - Prints all possible combinations of two different digits
 *
-* Return: Always 0 (Success)
+* Return: Always 0 (Succes)
 */
 
 int main(void)
 {
 int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit++)
+for (digit1 = 0; digit1 < 7; digit++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1; digit2 < 8; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-
-if (digit1 == 8 && digit2 == 9)
+if (digit1 == 6 && digit2 == 7)
 continue;
 putchar(',');
 putchar(' ');
