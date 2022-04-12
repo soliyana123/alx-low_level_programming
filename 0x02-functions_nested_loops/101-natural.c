3636eadfbb25ef68707577049d0e551234b7bcfd
soliye <soliy@gmail.com>
2022-04-12 06:23:50 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples of
- * 3 or 5 below 1024.
+ * main - computes and prints the sum of all the multiples of 3 or 5 below 1024.
+ * 
  * Return: Nothing.
  */
 
