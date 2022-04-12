38a3d30ee530cdccecd6e59d2f2fa2ae5a2fd26e
soliye <soliy@gmail.com>
2022-04-12 01:21:40 -0700

    task 10 0x02.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ * @i: print int i
+ * @l: print int l
+ * Return: Always 0.
+ */
+
+int add(int i, intl)
+{
+	return (i + l);
+}
+
