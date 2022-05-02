2ab27eb4aa670af36cb8aa509bdc65add1b5393e
soliye <soliy@gmail.com>
2022-05-02 12:54:15 -0700

    grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * free_grid - frees a 2 dimensional grid 
+/**  
+ * free_grid - frees a 2 dimensional grid previously created by
+ * alloc_grid function.
  * @grid: matrix double pointer
  * @height: rows (pointer)
  *
