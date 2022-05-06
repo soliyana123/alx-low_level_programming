7d8019a00088b6f6dcb1e4f6906689364de8a80a
soliye <soliy@gmail.com>
2022-05-06 05:49:17 -0700

    multiple of two number

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -129,9 +129,9 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	for (len1 = 0; argv[1][len1]; len1++)
-	{
+	;
 	for (len2 = 0; argv[2][len2]; len2++)
-	{
+	;
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 	if (tabres == NULL)
@@ -148,6 +148,4 @@ int main(int argc, char *argv[])
 	free(tabres);
 	exit(EXIT_SUCCESS);
 	return (0);
-}
-}
 }
