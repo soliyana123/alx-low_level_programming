fb3cfd4fabbbb95edd9aa4cc2e5d0fecca145b23
soliye <soliy@gmail.com>
2022-05-11 05:48:33 -0700

    five  functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,60 +1,60 @@
 #include "3-calc.h"
 
 /**
- * op_add - calculates the sum of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: sum of a and b
+ * op_add - funtion to add
+ * @a: input 1
+ * @b: input 2
+ * Return: sum a and b
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - calculates the difference of two integers
- * @a: first integer
- * @b: second integer
- *
+ * op_sub - funtion to subtract
+ * @a : input 1
+ * @b: input 2
  * Return: difference of a and b
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - calculates the product of two integers
- * @a: first integer
- * @b: second integer
- *
+ * op_mul - funtion to multiplies
+ * @a: input 1
+ * @b: input 2
  * Return: product of a and b
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - calculates the division of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: result of the division of a and b
+ * op_div - function to div
+ * @a: input 1
+ * @b: input 2
+ * Return: division of a by b
  */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
 /**
- * op_mod - calculates the remainder of the division of two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: remainder of a divided b
+ * op_mod - function to mod
+ * @a: input 1
+ * @b: input 2
+ * Return: remainder of the division of a
  */
+
 int op_mod(int a, int b)
 {
 	return (a % b);
