1c4a533d178a4983a0a97d4ecc294a4388f726c0
soliye <soliy@gmail.com>
2022-05-11 05:52:14 -0700

    function that selects the correct function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * get_op_func - function pointer that selects the correct function to perform
- * the operation asked by the user
- * @s: the operator given by the user
- *
- * Return: pointer to the function that corresponds to the
- * operator given as a parameter
+ * get_op_func - selects the correct func to perform the operation
+ * sked by the user
+ * @s: operator passed as arguement to program
+ * Return: a pointer to the function
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,14 +22,14 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+		if (*s == *(ops[i]).op)
+		{
+			return (*(ops[i]).f);
+		}
 		i++;
 	}
-
 	return (NULL);
 }
 ~
