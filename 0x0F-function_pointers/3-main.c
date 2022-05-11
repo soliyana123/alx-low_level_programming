a178bb919a13598867391a6463ee0da9ecddc429
soliye <soliy@gmail.com>
2022-05-11 05:55:09 -0700

    main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,46 +1,44 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
  */
+
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	char *sign;
+	char operator;
+	int a, b, answer;
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	operator = argv[2][0];
+	if ((operator != '*' && operator != '+' && operator != '-'
+	     && operator != '/'  && operator != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((operator == '/' || operator == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	sign = argv[2];
+	ptr = get_op_func(sign);
+	answer = (*ptr)(a, b);
+	printf("%d\n", answer);
 	return (0);
 }
