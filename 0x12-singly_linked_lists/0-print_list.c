4c76dba04ae19016e555932b3044c354d195a034
soliye <soliy@gmail.com>
2022-05-19 05:37:12 -0700

    prints all the elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,24 +8,26 @@
  * Return: number of nodes
  */
 
+
 size_t print_list(const list_t *h)
 {
-size_t cont = 0;
+	size_t cont = 0;
 
-while (h != NULL)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-h = h->next;
-cont++;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			cont++;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			cont++;
+		}
+	}
+	return (cont);
 }
-else
-{
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-cont++;
-}
-}
-return (cont);
-}
+
