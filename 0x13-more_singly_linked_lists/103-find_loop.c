64c73f9ea98c571ede68afcb60ba65eb83ab576c
soliye <soliy@gmail.com>
2022-05-24 04:07:54 -0700

    task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -17,7 +17,7 @@ listint_t *find_listint_loop(listint_t *head)
     if (!head)
         return (NULL);
 
-    while (slow && fast && fast->next)
+    while (slow&& fast && fast->next)
     {
         fast = fast->next->next;
         slow = slow->next;
