1c40409b95d3ec18ea0357e80b02c123c7fc79a3
soliye <soliy@gmail.com>
2022-05-26 04:29:27 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,48 +2,28 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
 
-int _pow_recursion(int x, int y)
+void _print_binary_(unsigned long int n)
 {
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	if (n)
+	{
+		_print_binary_(n >> 1);
+		_putchar((n & 1) + '0');
+	}
 }
-
 /**
- * _print_binary_ - convert an integer to binary but not zero
+ * print_binary - prints the binary
  * @n: integer
- *
- * Return: nothing
+ * Description: prints the binary representation of a number.
+ * Return: 1
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned int res_pow = 0;
-	int exp = 10;
-	int flag = 0;
-
-	if (n == 0)
+	if (n)
+		_print_binary_(n);
+	else
 		_putchar('0');
-	while (exp >= 0)
-	{
-		res_pow = _pow_recursion(2, exp);
-		if (n >= res_pow)
-		{
-			_putchar('1');
-			n -= res_pow;
-			flag = 1;
-		}
-		else if (n < res_pow && flag == 1)
-			_putchar('0');
-		exp--;
-	}
 }
