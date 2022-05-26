3b4ed056e20edc98704b0fc369a3d92340f75f77
soliye <soliy@gmail.com>
2022-05-26 04:13:52 -0700

    first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint( const char *b)
+{
+ unsigned int k = 1;
+ unsigned int i=0;
+ int c;
+ unsigned int len;
+
+ len = strlen(b);
+
+  for (c = len-1; c >= 0; c--)
+ {
+  if (b[c] != '0' && b[c] != '1')
+      return (0);
+    if (b[c] == '1')
+    {
+     i += k;
+     }
+    k *= 2;
+}
+ return (i);
+}
