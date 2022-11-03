6ddece8c5f7c77f8f60afc5822d98d3e7ccd3465
soliye <soliy@gmail.com>
2022-11-02 23:59:13 -0700

    search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,18 +1,15 @@
-#include "search_algos.h" 
- 
- 
- /** 
-   * binary_search - Searches for a value in a sorted array 
-   *                 of integers using binary search. 
-   * @array: A pointer to the first element of the array to search. 
-   * @size: The number of elements in the array. 
-   * @value: The value to search for. 
-   * 
-   * Return: If the value is not present or the array is NULL, -1. 
-   *         Otherwise, the index where the value is located. 
-   * 
-   * Description: Prints the [sub]array being searched after each change. 
-   */ 
+#include "search_algos.h"
+
+/**
+ * binary_search - function that searches for a value in a sorted array of
+ * integers using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value:  the value to search for
+ * Return: index where value is located
+ * If value is not present in array or if array is NULL, return -1
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
 	unsigned int lowerLimit = 0;
@@ -33,17 +30,14 @@ int binary_search(int *array, size_t size, int value)
 				printf("\n");
 		}
 		middle = (lowerLimit + upperLimit) / 2;
-		if (arrray[middle] == value)
+		if (array[middle] == value)
 			return (middle);
 		if (array[middle] < value)
 			lowerLimit = middle + 1;
 		if (array[middle] > value)
 			upperLimit = middle - 1;
-
- 	} 
- 
- 
- 	return (-1); 
- } 
+	}
+	return (-1);
+} 
 
 
